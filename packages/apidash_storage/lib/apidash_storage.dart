18166e5179e485f63f5302ea0b833c1f1d87dc11
Bhavesh-More <bhaveshmmore2006@gmail.com>
2026-03-27 14:36:38 +0530

    added HisService in apidash_storage

--- packages/apidash_storage/lib/apidash_storage.dart
+++ packages/apidash_storage/lib/apidash_storage.dart
@@ -1,5 +1,6 @@
 library apidash_storage;
 
+export 'src/his_service.dart';
 export 'src/services/collection_service.dart';
 export 'src/services/file_service.dart';
 export 'src/services/request_service.dart';
